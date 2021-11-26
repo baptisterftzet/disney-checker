ec2ff99804911d7eac9bf8b2cc3c523fc25cdaba
Thomas Moors <thomas.moors@proautnorm.com>
2021-11-26 14:52:12 +0100

    init

---
+++ DisneyChecker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DisneyChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = Console.ReadLine();
+        }
+    }
+}
